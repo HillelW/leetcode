977d8e392201b655fede77a75fe21a444d6a5ce9
hillelw <hillelwolin@gmail.com>
2023-03-23 14:12:30 -0400

    add assertion.

--- random/roman_to_int.py
+++ random/roman_to_int.py
@@ -1,13 +1,17 @@
 def roman_numeral_string_to_integer(roman_numeral: str) -> int:
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     n = len(roman_numeral)
     result = roman_map[roman_numeral[n - 1]]
+
     for i in range(n - 2, -1, -1):
         current_number = roman_map[roman_numeral[i]]
         previous_number = roman_map[roman_numeral[i + 1]]
-        
+
         if roman_map[roman_numeral[i]] >= previous_number:
             result += current_number
         else:
             result -= current_number
     return result
+
+
+assert roman_numeral_string_to_integer("XIV") == 14
